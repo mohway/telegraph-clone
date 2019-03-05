b3150fb69dd55e929678ecdb1572a54a7ac90ab9
naturalone <xnorish@gmail.com>
2019-03-05 22:56:19 +0200

    small updates

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,9 +1,38 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import uuid from "uuid";
+import "./Posts.css";
+import { Card } from "react-bootstrap";
 
 export class Posts extends Component {
   render() {
-    return <div />;
+    console.log(this.props.posts.posts);
+
+    const getPosts = this.props.posts.posts.map(post => (
+      <Card>
+        <Card.Body>
+          <div key={uuid()}>
+            <h3>{post.title}</h3>
+            <p>{post.body}</p>
+          </div>
+        </Card.Body>
+      </Card>
+    ));
+
+    const content =
+      getPosts.length === 0 ? <h1>No posts here yet.</h1> : getPosts;
+
+    return <div className="container posts">{content}</div>;
   }
 }
 
-export default Posts;
+const mapStateToProps = state => {
+  return {
+    posts: state.posts
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  null
+)(Posts);
